1e73f24f835901e29f9f7e6a53ca78228b3c8c9f
Connor Archibald <connor.archibald@Connors-MacBook-Air.local>
2025-05-22 09:06:42 +0100

    Wed Code Along afternoon tasks

--- src/main/scala/WedCodeAlong.scala
+++ src/main/scala/WedCodeAlong.scala
@@ -102,25 +102,98 @@ object WedCodeAlong extends App {
   println(name)
   println(emptyName)
 
-//getOrElse is used when we want a default value if  None is returned and it will take the Some() away leaving us with just the value
+  //getOrElse is used when we want a default value if  None is returned and it will take the Some() away leaving us with just the value
   def getName(name: Option[String]): String = name.getOrElse("This user has left the field empty")
 
   println(getName(name))
   println(getName(emptyName))
 
-  try{
+  try {
     val number = "one".toInt
     println(s"The number is $number")
-  } catch{
+  } catch {
     case e: Exception => println(s"$e was not a valid input")
 
   }
 
+  // EXERCISES
+
+  //1.
+  val numberOfMelons: Int = 6
+  val hasBag: Boolean = false
+
+  if (numberOfMelons <= 0) {
+    println(s"$numberOfMelons is not a valid number of watermelons")
+  } else {
+    val message: String = if (numberOfMelons <= 3 && !hasBag)
+      s"You don't need a bag to carry $numberOfMelons watermelons"
+    else if (numberOfMelons <= 3 && hasBag)
+      s"You have a bag but you could carry $numberOfMelons watermelons without it"
+    else if (numberOfMelons <= 5 && hasBag)
+      s"You have a bag which is needed to carry $numberOfMelons watermelons"
+    else if (numberOfMelons <= 5 && !hasBag)
+      s"You can't carry $numberOfMelons watermelons without a bag"
+    else
+      s"You can't carry $numberOfMelons watermelons"
+
+    println(message)
+
+    val maxCarry: Int = if (hasBag) 5 else 3
+    if(numberOfMelons <= maxCarry){
+      println(s"Yes you can buy $numberOfMelons watermelons")
+    }
+    else{
+      println(s"No you can't buy $numberOfMelons watermelons")
+    }
+
+  }
+
+  //2.
+  def calculatePay(input: String): String={
+    try {
+      val miles: Double = input.toDouble
+      if(miles < 0){
+        throw new Exception(s"$miles")
+      }
+      val pay:Double = miles * 0.45
+      s"for $miles you get payed £$pay"
+    } catch{
+      case e: Exception => s"$e is not a valid input please use a number above 0"
+    }
+  }
+  println(calculatePay("14"))
+  println(calculatePay("ten"))
+
+  //EXTENSION
+
+  //1.
+  // Order: (high -> low): *, /, %, +, -, :, <, >, =, !, &, ^, |, (letters), $, _
+
+  //2.
+  val test1: Boolean = (true || false && false) //TRUE
+  println("Test1: " + test1)
+
+  //3.
+  val test2: Boolean = ((true || false) && false) //FALSE
+  println("Test2: " + test1)
+
+  //4.
+  val test3: Boolean = (1 < 4 && 7 != 10 || 9 + 10 == 21) //TRUE
+  println("Test3: " + test1)
 
 
+  //RESEARCH
 
+  //1.
+  val first: Int = 2
+  val second: Int = 3
 
+  //2.
+  val answer: String =
+    if(first > second) "yes"
+    else "no"
 
+  println(s"First > Second: $answer")
 }
 
 
