5892874cc37a40280c5ba161beed9a11088f838e
Connor Archibald <connor.archibald@Connors-MacBook-Air.local>
2025-06-10 16:16:19 +0100

    Afternoon Task2

--- src/main/scala/Automation/Week5Day2/Task2.scala
+++ src/main/scala/Automation/Week5Day2/Task2.scala
@@ -1,5 +1,38 @@
 package Automation.Week5Day2
 
-object Task2 {
+
+import org.openqa.selenium.{By, WebDriver, WebElement}
+import org.openqa.selenium.chrome.ChromeDriver
+
+object Task2 extends App {
+
+  import org.openqa.selenium.chrome.ChromeDriver
+  import org.openqa.selenium.{By, WebDriver, WebElement}
+
+
+  val driver: WebDriver = new ChromeDriver
+  driver.get("https://demoqa.com/automation-practice-form")
+
+  driver.navigate().to("https://example.com")
+  driver.navigate().back()
+  driver.navigate().forward()
+  driver.navigate().refresh()
+
+  driver.manage().window().minimize()
+  driver.manage().window().maximize()
+
+
+
+
+  println(s"The is part of the page source ${driver.getPageSource.take(30)}")
+
+
+
+
+
+  // driver.close()
+
+   driver.quit()
 
 }
+
--- src/main/scala/Automation/Week5Day2/Task3.scala
+++ src/main/scala/Automation/Week5Day2/Task3.scala
@@ -1,5 +1,41 @@
 package Automation.Week5Day2
 
-object Task3 {
 
+import org.openqa.selenium.{By, WebDriver, WebElement}
+import org.openqa.selenium.chrome.ChromeDriver
+
+object Task3 extends App {
+
+  import org.openqa.selenium.chrome.ChromeDriver
+  import org.openqa.selenium.{By, WebDriver, WebElement}
+
+
+  val driver: WebDriver = new ChromeDriver
+  driver.get("https://demoqa.com/automation-practice-form")
+  driver.manage().window().maximize()
+
+  val firstName: WebElement = driver.findElement(By.id("firstName"))
+  //  val submitButton: WebElement = driver.findElement(By.xpath("//*[@id='submit']"))
+  val readingCheckbox = driver.findElement(By.xpath("//label[@for='hobbies-checkbox-2']"))
+
+  if (firstName.isEnabled && firstName.isDisplayed) {
+    firstName.sendKeys("Alice")
+    println("Firstname field filled out")
+  }
+
+  if (readingCheckbox.isDisplayed && readingCheckbox.isEnabled && !readingCheckbox.isSelected) {
+    readingCheckbox.click()
+    println("Checkbox has been clicked")
+  }
+
+  //  if (submitButton.isDisplayed && submitButton.isEnabled) {
+  //    submitButton.click()
+  //    println("Submit button has been clicked")
+  //  }
+
+
+  // driver.close()
+
+   driver.quit()
 }
+
